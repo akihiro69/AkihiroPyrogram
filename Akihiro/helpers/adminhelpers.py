3dd679a40a2d1dc3f92227610fa1132fca6239ff
akihirooo <78153278+akihiro69@users.noreply.github.com>
2022-06-17 20:08:01 +0700

    Add files via upload

---
+++ Akihiro/helpers/adminhelpers.py
@@ -0,0 +1,37 @@
+# Copyright (C) 2020-2021 by okay-retard@Github, < https://github.com/okay-retard >.
+#
+# This file is part of < https://github.com/okay-retard/ZectUserBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/okay-retard/ZectUserBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from time import sleep, time
+
+from pyrogram.types import Message
+
+from Zect import app
+
+
+async def CheckAdmin(message: Message):
+    """Check if we are an admin."""
+    admin = "administrator"
+    creator = "creator"
+    ranks = [admin, creator]
+
+    SELF = await app.get_chat_member(
+        chat_id=message.chat.id, user_id=message.from_user.id
+    )
+
+    if SELF.status not in ranks:
+        await message.edit("__I'm not Admin!__")
+        sleep(2)
+        await message.delete()
+
+    else:
+        if SELF.status is not admin or SELF.can_restrict_members:
+            return True
+        else:
+            await message.edit("__No Permissions to restrict Members__")
+            sleep(2)
+            await message.delete()
