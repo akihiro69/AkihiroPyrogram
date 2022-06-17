9db5aadbb2fb7965fd465ce2beb2360d9ffe9467
akihirooo <78153278+akihiro69@users.noreply.github.com>
2022-06-18 06:34:33 +0700

    Update kang.py

--- Akihiro/modules/kang.py
+++ Akihiro/modules/kang.py
@@ -16,8 +16,8 @@ from pyrogram import emoji, filters
 from pyrogram.raw.functions.messages import GetStickerSet
 from pyrogram.raw.types import InputStickerSetShortName
 from pyrogram.errors import YouBlockedUser, StickersetInvalid
-from Zect.helpers.pyrohelper import get_args
-from Zect import app, CMD_HELP
+from Akihiro.helpers.pyrohelper import get_args
+from Akihiro import app, CMD_HELP
 from config import PREFIX
 
 CMD_HELP.update(
