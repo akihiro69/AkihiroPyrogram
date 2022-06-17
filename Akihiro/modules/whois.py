8b3f76865e4a5e02411971592623e9a8f4bfc8f0
akihirooo <78153278+akihiro69@users.noreply.github.com>
2022-06-18 06:45:25 +0700

    Update whois.py

--- Akihiro/modules/whois.py
+++ Akihiro/modules/whois.py
@@ -13,7 +13,7 @@ from pyrogram import filters
 from pyrogram.types import User, InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.raw import functions
 from pyrogram.errors import PeerIdInvalid
-from Zect import app
+from Akihiro import app
 from config import PREFIX
 
 
